bdfbc54925c93558f1fd87eb566db46527f5787c
varliche <maxime.var@gmail.com>
2022-04-01 20:44:24 +0200

    adding fixtures for users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,23 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 5; $i++) {
+            $user = new User();
+            $user->setFirstName('user' . $i);
+            $user->setLastName('kanban');
+            $user->setEmail('user' . $i . 'kanban@mail.test.com');
+            $user->setPassword('1234kanbanuser' . $i);
+            $user->setCreatedOn(new \DateTime());
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
